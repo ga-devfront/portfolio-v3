3a6ad10e1e11c506006db1c4a3692d7a1febdbe3
Alexis Guyomar <alexis.guyomar@prestashop.com>
2024-02-15 14:12:01 +0100

    feat: add i18n for translation

--- portfolio-front/nuxt.config.ts
+++ portfolio-front/nuxt.config.ts
@@ -1,7 +1,32 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', '@nuxtjs/i18n'],
+  i18n: {
+    strategy: 'prefix_and_default',
+    langDir: 'locales/',
+    lazy: true,
+    baseUrl: process.env.NUXT_BASE_URL,
+    defaultLocale: 'en',
+    locales: [
+      {
+        name: 'English',
+        code: 'en',
+        iso: 'en',
+        file: 'en/index.ts'
+      },
+      {
+        name: 'Français',
+        code: 'fr',
+        iso: 'fr',
+        file: 'fr/index.ts'
+      }
+    ],
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root'
+    }
+  },
   css: [
     'assets/styles/index.scss'
   ]
