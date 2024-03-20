ab9cf1bf07b93e17d529b0fc03a7118e17bc57b1
Alexis Guyomar <alexis.guyomar@prestashop.com>
2024-03-20 09:02:16 +0100

    feat: add and adjust background

--- portfolio-front/components/layout/FixedBackground.vue
+++ portfolio-front/components/layout/FixedBackground.vue
@@ -1,5 +1,15 @@
 <template>
-  <div id="background" />
+  <div aria-hidden="true" id="background">
+    <div class="background__gradiant"></div>
+    <svg class="background__grid">
+      <defs>
+        <pattern id="gridRect" width="60" height="60" x="50%" y="-1" patternUnits="userSpaceOnUse">
+          <path d="M.5 200V.5H200" fill="none"></path>
+        </pattern>
+      </defs>
+      <rect width="100%" height="100%" stroke-width="0" fill="url(#gridRect)"></rect>
+    </svg>
+  </div>
 </template>
 <script setup lang="ts">
 </script>
